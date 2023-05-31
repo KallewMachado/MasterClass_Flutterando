591319bb50713962169a4bc2be0f7984dc4357f4
Kallew Machado <kallewcarlos@gmail.com>
2023-05-31 11:37:53 -0300

    feat: fix name files

--- bin/main_imc.dart
+++ bin/main_imc.dart
@@ -1,8 +1,7 @@
-import 'package:dart_playground/task01/imc.dart';
+import 'package:dart_playground/task01/calc_imc.dart';
 
 void main(List<String> arguments) {
   Pessoa pessoa = Pessoa('Kallew Machado', 1.71, 63);
 
-  // print('Hello World!');
   pessoa.calcIMC();
 }
