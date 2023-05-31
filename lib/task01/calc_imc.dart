054d130e63de06b2cb452b4afffb589b19929b9e
Kallew Machado <kallewcarlos@gmail.com>
2023-05-31 11:49:05 -0300

    fix: fix name variable

--- lib/task01/calc_imc.dart
+++ lib/task01/calc_imc.dart
@@ -1,9 +1,9 @@
 class Pessoa {
-  final String nome;
+  final String name;
   final double height;
   final double weight;
 
-  Pessoa(this.nome, this.height, this.weight);
+  Pessoa(this.name, this.height, this.weight);
 
   calcIMC() {
     var result = weight / (height * height);
@@ -11,16 +11,16 @@ class Pessoa {
     print('Seu IMC: ${result.toStringAsFixed(2)}');
 
     if (result <= 18.4) {
-      print('$nome você esta abaixo do peso');
+      print('$name você esta abaixo do peso');
     }
     if (result < 24.9 && result > 18.4) {
-      print('$nome você esta no peso ideal');
+      print('$name você esta no peso ideal');
     }
     if (result < 29.9 && result > 24.9) {
-      print('$nome você esta levemente acima do peso');
+      print('$name você esta levemente acima do peso');
     }
     if (result > 29.9) {
-      print('$nome você esta acima do peso');
+      print('$name você esta acima do peso');
     }
   }
 }
